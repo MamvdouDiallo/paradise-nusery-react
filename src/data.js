75acfbcb6a7ba59e47345745db6f02a7f06cab0a
dmamadou <mamadou.diallo@nevorus.com>
2025-08-13 13:50:19 +0000

    mise ajour

--- src/data.js
+++ src/data.js
@@ -4,41 +4,41 @@ export const plants = [
     name: "Monstera Deliciosa",
     price: 29.99,
     category: "Popular",
-    image: "/images/background.jpeg"
+    image: "/images/fruit1.jpg"
   },
   {
     id: 2,
     name: "Fiddle Leaf Fig",
     price: 39.99,
     category: "Popular",
-    image: "/images/background.jpeg"
+    image: "/images/fruit2.jpg"
   },
   {
     id: 3,
     name: "Snake Plant",
     price: 19.99,
     category: "Low Maintenance",
-    image: "/images/background.jpeg"
+    image: "/images/fruit3.jpg"
   },
   {
     id: 4,
     name: "Peace Lily",
     price: 24.99,
     category: "Flowering",
-    image: "/images/background.jpeg"
+    image: "/images/fruit4.jpg"
   },
   {
     id: 5,
     name: "ZZ Plant",
     price: 22.99,
     category: "Low Maintenance",
-    image: "/images/background.jpeg"
+    image: "/images/fruit5.jpg"
   },
   {
     id: 6,
     name: "Pothos",
     price: 14.99,
     category: "Beginner Friendly",
-    image: "/images/background.jpeg"
+    image: "/images/fruit6.jpg"
   }
 ];
